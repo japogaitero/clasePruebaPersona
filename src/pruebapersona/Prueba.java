5dfeabc7e9aa04e84ac83d2e90d717db5f28eaba
Japogaitero <japogaitero@gmail.com>
2021-02-12 12:22:13 +0100

    segundo commit

--- src/pruebapersona/Prueba.java
+++ src/pruebapersona/Prueba.java
@@ -1,4 +1,4 @@
-/*
+   /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -11,5 +11,7 @@ package pruebapersona;
  * @author Victor Alvarez <japogaitero at gmail.com>
  */
 public class Prueba {
+    // comentarios de prueba desde el primer commit
 
 }
+ 
